0e060079c591b0038e1d98fe20625432bcd92de5
Cecep Suwanda <Cecepsuwanda@yahoo.com>
2019-04-20 06:03:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,18 +15,25 @@
 
   function isi_data(&$data,$url_wilayah,$url_jml,$idx){
 
-    $tmp=array('kab_kota','kec','lurah');  
+    $tmp=array('kab_kota','kec','lurah','tps');  
     
     $json_wilayah = get_json($url_wilayah);
     $json_jml = get_json($url_jml);   
     
+    
+
     if(!empty($json_wilayah)){
 	    foreach ($json_wilayah as $key_wilayah=>$wilayah) {  
+	       $rec = array();
+	       $rec['kode']=$key_wilayah;
+	       $rec['nama']=$wilayah->nama;
 	       //$data[$key_wilayah]['nama']=$wilayah->nama;
+	       $rec['jml_suara']=array();
 	       if(!empty($json_jml)){
 	        if(isset($json_jml->table->$key_wilayah)){
 	         foreach ($json_jml->table->$key_wilayah as $key_suara => $value) {
 	     	       //$data[$key_wilayah]['jml_suara'][$key_suara]=$value;
+	         	   $rec['jml_suara'][$key_suara]=$value;
 	         } 
             }
 	       }
@@ -36,7 +43,7 @@
 	          $url_jml = 'https://pemilu2019.kpu.go.id/static/json/hhcw/ppwp/'.$key_wilayah.'.json';
 	          isi_data($data[$key_wilayah][$tmp[$idx]],$url_wilayah,$url_jml,$idx+1); 
               
-	       }elseif($idx<=1){
+	       }elseif($idx<=3){
 
 	          $url_path = parse_url($url_wilayah, PHP_URL_PATH);
 	          $dirname = pathinfo($url_wilayah, PATHINFO_DIRNAME);
